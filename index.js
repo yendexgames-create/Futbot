d6afeb81c74595ab92f4cef662f1a992107561f8
yendexgames-create <yendexgames@gmail.com>
2025-11-25 19:29:59 +0500

    skss

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+
+// Add this at the very top of index.js
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  process.exit(1);
+});
 const { Telegraf, Markup } = require('telegraf');
 const connectDB = require('./database');
 const User = require('./models/User');
